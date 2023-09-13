83918589dfaea1aef9c41317016bd6d7c9ee9782
ramzan <93776325+ramzanka06@users.noreply.github.com>
2023-09-13 13:39:33 +0300

    Реализовал статистику пользователей (Макет)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import Todos from './components/Todos/Todos';
 import Footer from './components/Footer/Footer';
 import Sliders from './components/Slider/Slider';;
 import './App.css'
+import Statistics from './components/Statistics-glob/Statistics';
 
 function App() {
 
   return (
     <>
+    <Statistics/>
       <Todos/>
       <Sliders/>
       <Footer/>
